649a74a7eed99d727057bbd8dbb2aea276afd95a
SIR-97 <sirafiq.1997@gmail.com>
2023-01-28 11:45:23 +0000

    working on adding questions, once the start button has been clicked

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,11 @@ var submitBtn = document.querySelector("#submit");
 var initialEl = document.querySelector("#initials");
 var feedbackEl = document.querySelector("#feedback");
 var startScreen = document.querySelector("#start-screen")
+
+// variables regarding the quiz
+
+let shuffledQuestions, currentQuestionIndex 
+
 // click to start game 
 
 startBtn.addEventListener("click", startGame)
@@ -19,9 +24,13 @@ function startGame(){
     startBtn.classList.add("hide")
     startScreen.classList.add("hide")
 
+    // allows the questions to appear randomly
+    shuffledQuestions = questions.sort(() => Math.random - .5) 
+    currentQuestionIndex = 0
+
     // uncovers questions page that is hidden 
     questionsEl.classList.remove("hide")
-
+    setNextQuestion()
 
 }
 
@@ -31,8 +40,29 @@ function setTimer(){
 
 function setNextQuestion(){
 
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+// create function that shows questions
+function showQuestion(question){
+    questionsEl.innertext = question.question
 }
 
 function selectAnswer(){
 
 }
+
+// created my questions on the fundementals of JavaScript 
+
+var questions = [
+    {
+        question: "what is JavaScript?",
+        answers: [
+            {text: "a programming software", correct: true },
+            {text: "a document", correct: false},
+
+        ]
+    }
+
+]
